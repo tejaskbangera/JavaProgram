7e25464486b467ecc663b4acfe42814fcda48ad5
tejaskbangera <tejasbangera1@gmail.com>
2019-10-28 11:39:40 +0530

    hello2 added

---
+++ hello2.java
@@ -0,0 +1,16 @@
+package com.smt.clover.classes;
+
+public class HelloWorld{
+
+	public static void main(String[] args)
+	{
+	   System.out.println("Hello World is here ...................");
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+	
+}
